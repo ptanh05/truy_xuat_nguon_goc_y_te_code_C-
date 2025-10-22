21250fe059b7720ae144461c8f1a67291248622d
ptanh05 <148223045+ptanh05@users.noreply.github.com>
2025-10-22 21:44:18 +0700

    commit

--- backend/obj/Debug/net8.0/PharmaDNA.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/PharmaDNA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaDNA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ecaf1e7b31c96a5c64bf7363549225adcad742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0641f15d16f5cb5c5e62cdb8717ee5d91c4c18f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaDNA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaDNA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
